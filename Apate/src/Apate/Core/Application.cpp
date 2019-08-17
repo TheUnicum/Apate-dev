358cf26f2542bc58c9e875b260dc1302c2f85045
Mattia <tia_ben@tin.it>
2019-08-17 18:45:34 +0200

    Dispatching events from GLFW.

--- Apate/src/Apate/Core/Application.cpp
+++ Apate/src/Apate/Core/Application.cpp
@@ -6,14 +6,25 @@
 
 namespace Apate {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		AP_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -26,6 +37,12 @@ namespace Apate {
 
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent & e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 }
--- Apate/src/Apate/Core/Application.h
+++ Apate/src/Apate/Core/Application.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "Apate/Core/Base.h"
+#include "Apate/Events/Event.h"
+#include "Apate/Events/ApplicationEvent.h"
+
 #include "Apate/Core/Window.h"
 
 namespace Apate {
@@ -12,7 +15,11 @@ namespace Apate {
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
